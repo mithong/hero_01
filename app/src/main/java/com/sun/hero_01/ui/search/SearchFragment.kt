01c1c67e25644d0e1142c94edb549f659c0dd229
Vinh con <nguyenthanhvinhql@gmail.com>
2021-05-20 16:50:58 +0700

    handle to go to search fragment

--- app/src/main/java/com/sun/hero_01/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sun/hero_01/ui/search/SearchFragment.kt
@@ -1,4 +1,18 @@
 package com.sun.hero_01.ui.search
 
-class SearchFragment {
+import androidx.core.os.bundleOf
+import com.sun.hero_01.R
+import com.sun.hero_01.base.BaseFragment
+
+class SearchFragment() : BaseFragment() {
+
+    override val layoutResourceId = R.layout.fragment_search
+
+    companion object {
+        private const val ARGUMENT_HERO_NAME = "ARGUMENT_HERO_NAME"
+
+        fun newInstance(keyHeroName: String) = SearchFragment().apply {
+            arguments = bundleOf(ARGUMENT_HERO_NAME to keyHeroName)
+        }
+    }
 }
