110cb64a831c8b6ea7e7e1f586574c7fab7cc604
Vinh con <nguyenthanhvinhql@gmail.com>
2021-06-01 09:39:04 +0700

    Unit test champion

--- app/src/test/java/com/sun/hero_01/FakeData.kt
+++ app/src/test/java/com/sun/hero_01/FakeData.kt
@@ -1,5 +1,6 @@
 package com.sun.hero_01
 
+import com.sun.hero_01.data.model.Hero
 import com.sun.hero_01.data.model.HeroDetail
 
 object FakeData {
@@ -19,5 +20,35 @@ object FakeData {
         spells = mutableListOf(),
         stats = null,
         isFavorite = false
+    )
+
+    val HEROES = mutableListOf(
+        Hero(
+            id = "Aatrox",
+            name = "Aatrox",
+            title = "the Darkin Blade",
+            difficulty = 4,
+            primaryTag = "Primary Tag",
+            secondaryTag = "Secondary Tag",
+            image = "Aatrox.png"
+        ),
+        Hero(
+            id = "Chogath",
+            name = "Cho'Gath",
+            title = "the Terror of the Void",
+            difficulty = 4,
+            primaryTag = "Primary Tag",
+            secondaryTag = "Secondary Tag",
+            image = "Chogath.png"
+        ),
+        Hero(
+            id = "Corki",
+            name = "Corki",
+            title = "the Daring Bombardier",
+            difficulty = 4,
+            primaryTag = "Primary Tag",
+            secondaryTag = "Secondary Tag",
+            image = "Corki.png"
+        )
     )
 }
